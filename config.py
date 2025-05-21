de8e3c89bdb89e0bb81afdbd04c6cb8ce1e511cc
PlayMaker1998 <sagitario10511@gmail.com>
2025-05-21 17:11:31 -0500

    Mejorar seguridad: SECRET_KEY con variable de entorno

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'tu_secreto_aqui'
+    # Obtener la clave secreta desde una variable de entorno
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_por_defecto')
+    
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'cafeteria.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     # Caché simple en memoria
     CACHE_TYPE = 'simple'
